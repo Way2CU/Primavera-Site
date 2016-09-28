8227f834a503d23617958d86943da70029963688
iliya <iliyuskeisar@gmail.com>
2016-09-28 18:07:09 +0300

    changed to main-primavera

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -44,15 +44,41 @@ Site.is_mobile = function() {
 
 	return result;
 };
-
 /**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
-};
 
+	//Page Control for Product_gallery
+	Site.product_gallery = new PageControl('section#product_gallery','div.thumb_large');
+	Site.product_gallery
+		.attachControls($('ul#images li'));
+
+	Site.tabs = new PageControl('section#information','div.content');
+	Site.tabs
+		.attachControls($('section#information div#tabs a'));
+
+	//create dynamic controls for testimonial page control
+	var testimonials = document.querySelectorAll('div.testemonials article');
+	var controls_container = document.querySelector('div.control_panel');
+	for(var i = 0; i < testimonials.length; i++) {
+		var link = document.createElement('a');
+		link.classList.add('control')
+		link.setAttribute('href', 'javascript:void(0)');
+		controls_container.appendChild(link);
+	}
+
+	// create page control for site testimonails
+	Site.testimonials = new PageControl('div.testemonials', 'article');
+	Site.testimonials
+		.setWrapAround(true)
+		.attachControls($('div.control_panel a.control'))
+		.attachPreviousControl($('a.prev'))
+		.attachNextControl($('a.next'));
+};
+		
 
 // connect document `load` event with handler function
 $(Site.on_load);
